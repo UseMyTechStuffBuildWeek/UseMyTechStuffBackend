d0acf0e79424f7c752e142e964a65351458b9208
DJcri <djcri123@gmail.com>
2021-04-23 23:51:02 -0700

    heroku push

--- knexfile.js
+++ knexfile.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+require("dotenv").config();
 /*
 
   PORT=5000
@@ -15,29 +15,32 @@ require('dotenv').config()
     - testing_database_name (use the real name of the testing database you created in pgAdmin 4)
 
 */
-const pg = require('pg')
+const pg = require("pg");
 
 if (process.env.DATABASE_URL) {
-  pg.defaults.ssl = { rejectUnauthorized: false }
+  pg.defaults.ssl = { rejectUnauthorized: false };
 }
 
 const sharedConfig = {
-  client: 'pg',
-  migrations: { directory: './api/data/migrations' },
-  seeds: { directory: './api/data/seeds' },
-}
+  client: "pg",
+  migrations: { directory: "./api/data/migrations" },
+  seeds: { directory: "./api/data/seeds" },
+};
 
 module.exports = {
   development: {
     ...sharedConfig,
     connection: {
-      host: 'localhost',
+      host: "localhost",
       port: 5432,
-      user: 'postgres',
-      password: 'postgres',
-      database: 'UseMyTech'
+      user: "postgres",
+      password: "postgres",
+      database: "UseMyTech",
     },
-    pool: { min: 2, max: 10 },
+    pool: {
+      min: 2,
+      max: 10,
+    },
     // connection: 'postgresql://postgres:postgres@localhost:5432/foodtruck',
     // connection: process.env.DEV_DATABASE_URL,
     useNullAsDefault: true,
@@ -51,4 +54,4 @@ module.exports = {
     connection: process.env.DATABASE_URL,
     pool: { min: 2, max: 10 },
   },
-}
+};
