7a5b1035af53ac528c94586c6684230415b3d51d
Liza Pincsak <lizapincsak@MacBook-Air-2.local>
2021-04-26 09:22:03 -0500

    trying to get tables working

--- api/data/seeds/04-requests.js
+++ api/data/seeds/04-requests.js
@@ -3,8 +3,9 @@ exports.seed = function (knex) {
     .del()
     .then(function () {
       return knex("requests").insert([
-        { request_id: 1, equipment_id: 1, user_id: 1, accepted: false },
-        { request_id: 3, equipment_id: 3, user_id: 3, accepted: true },
+        { equipment_id: 1, user_id: 1, accepted: false },
+        { equipment_id: 3, user_id: 3, accepted: true },
+        { equipment_id: 2, user_id: 3, accepted: true },
       ]);
     });
 };
